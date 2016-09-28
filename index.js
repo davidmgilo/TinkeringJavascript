e95ead8474a9a9e98c499f6f6ee3ea3215350fdd
David Martinez <davidmgilo@gmail.com>
2016-09-28 19:52:10 +0200

    window object

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
 console.log("Hello world at external file");
 
-alert('Hola mon!'); //Evitar utilitzar aquesta forma!
+//alert('Hola mon!'); //Evitar utilitzar aquesta forma!
+
+$name= 'David';
+
+//Windows: només en navegadors!!
+window.alert('Hola mon!')
+
+//Objecte window : objecte per defecte en navegador = Namespace Global
+//Tothom depèn d'aquest objecte.
