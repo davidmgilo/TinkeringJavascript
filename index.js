d727890fcd3c988b94b2ec8e2f3dec2c93798f87
David Martinez <davidmgilo@gmail.com>
2016-09-30 16:32:26 +0200

    json

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 //NAMESPACES
 //DE MOMENT no tenim class {}
+
+// var myApp = [] -> Arrays
+
+//FRONTEND: Javascript
+
+//BACKEND: Java, PHP, JavaScript, Ruby, Python
+
+//JavaScript Object Notation aka JSON
 var myApp = {}; //Objecte buit
 
