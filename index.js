c225fd589d8c7e7ce022f7f84e4c938ec0765f7f
David Martinez <davidmgilo@gmail.com>
2016-09-30 16:08:09 +0200

    hello v

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-//VARIABLE GLOBALS: un problema. Raó: els xocs entre fitxers perquè tenen variables globals amb el mateix nom.
-
-$a =
-
-
-function (){
-    $a ="";
-    //Variables locals
+function hello() {
+    $name = "David";
+    console.log("Hola " + $name);
 }
 
-console.log($a);//No funcionarà, està forma del seu àmbit.
+hello();
 
 //JAVASCRIPT STRICT MODE: No l'activeu
 //SCOPE: ÂMBIT DE VIDA. Zona del codi on una variable té vida o sentit. Quan comença i quan s'acaba una variable
