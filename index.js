09856a9645ab20a2570e0e60f12513f74fadfc1f
David Martinez <davidmgilo@gmail.com>
2016-09-30 16:14:53 +0200

    var foo

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function hello(bool) {
     // $name = "David";
     // console.log("Hola " + $name);
     if(bool){
-        foo = "bar";
+        var foo = "bar";
         console.log(foo)
     }else{
-        console.log(foo); 
+        console.log(foo);
     }
 }
 
