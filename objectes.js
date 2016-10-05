344eb79ad6baa44a613df20c401b8075927b4cbf
David Martinez <davidmgilo@gmail.com>
2016-10-05 19:56:12 +0200

    No new

--- objectes.js
+++ objectes.js
@@ -6,12 +6,22 @@
 
 //Javascript: functional programming
 
-function Persona(name) {
-    console.log('Creant una nova persona')
-    this.name = name;
-    this.sn1 ='Martinez';
-} // function contructor
+// function Persona(name) {
+//     console.log('Creant una nova persona')
+//     this.name = name;
+//     this.sn1 ='Martinez';
+// } // function contructor
+//
+// var persona1 = new Persona('Xavier');
+// console.log(persona1);
 
-var persona1 = new Persona('Xavier');
-console.log(persona1);
+var persona = function() {
+   console.log('Creant una nova persona');
+     this.name = 'David';
+     this.sn1 ='Martinez';
+};
 
+console.log (persona.name); // Així no funciona, necessita el new o el bind...
+
+//bind
+//per especificar a que fem referencia quan diem this
