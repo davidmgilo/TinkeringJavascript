ece0a8fd1f5dea9ffd820a81dc97f5911325bcc6
David Martinez <davidmgilo@gmail.com>
2016-10-05 19:39:43 +0200

    Functions Constructors

---
+++ objectes.js
@@ -0,0 +1,15 @@
+// class { } -> Sugar Syntax
+
+// First class citizens functions
+//var x = function(){}
+// Utilitzarem les funcions per "fer" objectes
+
+//Javascript: functional programming
+
+function Persona() {
+
+} // function contructor
+
+var persona1 = new Persona();
+var persona2 = new Persona();
+
